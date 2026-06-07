168b2ae4c63f903141efd481855f8bf8efb2fb79
gitfox-enter <156575760+gitfox-enter@users.noreply.github.com>
2026-06-07 12:56:08 +0800

    Update logo to momo pink dinosaur avatar

--- src/site.config.ts
+++ src/site.config.ts
@@ -26,7 +26,7 @@ export const theme: ThemeUserConfig = {
   },
   /** Set a logo image to show in the homepage. */
   logo: {
-    src: '/src/assets/fox-icon.svg',
+    src: '/images/momo-avatar.jpg',
     alt: 'Avatar'
   },
 
