d70c26802742a18c95bd090db08352ebca50c6b0
gitfox-enter <156575760+gitfox-enter@users.noreply.github.com>
2026-06-11 10:36:25 +0800

    fix: remove /gold-bear-blog base path from public/sw-register.js

--- public/sw-register.js
+++ public/sw-register.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
-      .register('/gold-bear-blog/sw.js')
+      .register('/sw.js')
       .then(registration => {
         console.log('[SW] Registered:', registration.scope);
       })
