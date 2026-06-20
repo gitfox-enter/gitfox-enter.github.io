1311945b2f1f298b964ad96ca5a682f960f939e5
gitfox-enter <156575760+gitfox-enter@users.noreply.github.com>
2026-06-20 21:00:05 +0800

    refactor: 移除导航栏文档分类

--- src/site.config.ts
+++ src/site.config.ts
@@ -64,7 +64,7 @@ export const theme: ThemeUserConfig = {
   header: {
     menu: [
       { title: '博客', link: '/blog' },
-      { title: '文档', link: '/docs' },
+      
       { title: '项目', link: '/projects' },
       { title: '链接', link: '/links' },
       { title: '关于', link: '/about' }
